5086ffe6bc95774b3dbfe805cf56fbcfe823b985
William Hughes <williamh@catalyst.net.nz>
2015-01-06 15:59:08 +1300

    Ensure debootstrap scripts exist for trusty and utopic

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,17 @@ class cowbuilder {
   package { 'cowbuilder':
     ensure => present
   }
+
+  file { '/usr/share/debootstrap/scripts/trusty':
+    ensure  => link,
+    target  => '/usr/share/debootstrap/scripts/gutsy',
+    require => Package['cowbuilder']
+  }
+
+  file { '/usr/share/debootstrap/scripts/utopic':
+    ensure  => link,
+    target  => '/usr/share/debootstrap/scripts/gutsy',
+    require => Package['cowbuilder']
+  }
+
 }
