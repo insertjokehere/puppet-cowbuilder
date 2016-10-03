9c6673c38c9275974dce6ae50666d4b27e570569
William Hughes <williamh@catalyst.net.nz>
2016-10-03 15:40:38 +1300

    Need to install qemu-user-binfmt to get qemu-user-static to work properly

--- manifests/base.pp
+++ manifests/base.pp
@@ -50,8 +50,8 @@ define cowbuilder::base(
 
   if ($arch != $::architecture or ($arch == 'i386' and $::architecture == 'amd64')) {
     $debootstrap = 'qemu-debootstrap'
-    $req = [Package['qemu-user-static'], Package['binfmt-support']]
-    ensure_packages(['qemu-user-static', 'binfmt-support'])
+    $req = [Package['qemu-user-static'], Package['binfmt-support'], Package['qemu-user-binfmt']]
+    ensure_packages(['qemu-user-static', 'binfmt-support', 'qemu-user-binfmt'])
   } else {
     $debootstrap = 'debootstrap'
     $req = []
