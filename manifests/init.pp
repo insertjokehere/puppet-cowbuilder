fc963da68440a75d024c3ecc0d86bd487f8b39f7
William Hughes <williamh@catalyst.net.nz>
2016-10-03 14:55:42 +1300

    Update cowbuilder

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,5 @@
 class cowbuilder {
-  package { 'cowbuilder':
+  package { ['cowbuilder', 'ubuntu-keyring', 'debian-keyring']:
     ensure => present
   }
-
-  file { '/usr/share/debootstrap/scripts/trusty':
-    ensure  => link,
-    target  => '/usr/share/debootstrap/scripts/gutsy',
-    require => Package['cowbuilder']
-  }
-
-  file { '/usr/share/debootstrap/scripts/utopic':
-    ensure  => link,
-    target  => '/usr/share/debootstrap/scripts/gutsy',
-    require => Package['cowbuilder']
-  }
-
 }
